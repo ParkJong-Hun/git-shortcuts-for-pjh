44d4064d821b16fdc2ceeaeea9d69b4231a74278
ParkJong-Hun <thesn515@gmail.com>
2025-10-07 08:28:25 +0900

    new aliases

--- README.md
+++ README.md
@@ -20,8 +20,11 @@ source ~/.bashrc # if using bash
 |-----------------|---------------|------------------|-------------|
 | `git-setup` | `pit -s` | `brew install git` (or apt-get, yum, etc.) | Install git using package manager |
 | `git-push-this` | `pit -p` | `git push origin {current-branch}` | Push current branch to origin |
+| `git-force-push-this` | `pit -pf` | `git push -f origin {current-branch}` | Force push current branch to origin (with confirmation) |
 | `git-commit-all "message"` | `pit -c "message"` | `git add . && git commit -m "message"` | Stage all changes and commit |
-| `git-reset-this` | `pit -r` | `git reset --hard HEAD` | Hard reset to HEAD (with confirmation) |
+| `git-reset-this` | `pit -r` | `git add . && git reset --hard HEAD` | Hard reset to HEAD (with confirmation) |
+| `git-new-branch "name"` | `pit -b "name"` | `git switch -c {name}` | Create and switch to a new branch |
+| `git-modify-branch "name"` | `pit -m "name"` | `git branch -M {name}` | Rename current branch |
 
 ## Usage Examples
 
@@ -41,10 +44,25 @@ git-push-this
 # or shorter
 pit -p
 
+# Force push current branch
+git-force-push-this
+# or shorter
+pit -pf
+
 # Discard all changes
 git-reset-this
 # or shorter
 pit -r
+
+# Create and switch to a new branch
+git-new-branch "feature/new-feature"
+# or shorter
+pit -b "feature/new-feature"
+
+# Rename current branch
+git-modify-branch "feature/updated-name"
+# or shorter
+pit -m "feature/updated-name"
 ```
 
 ## Uninstallation
--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -10,14 +10,20 @@ Git Shortcuts - Available Commands
 FULL COMMANDS:
   git-setup              Install git using package manager
   git-push-this          Push current branch to origin
+  git-force-push-this    Force push current branch to origin (with confirmation)
   git-commit-all "msg"   Stage all changes and commit with message
   git-reset-this         Hard reset to HEAD (with confirmation)
+  git-new-branch "name"  Create and switch to a new branch
+  git-modify-branch "name"  Rename current branch
 
 SHORT ALIASES:
   pit -s                 Same as git-setup
   pit -p                 Same as git-push-this
+  pit -pf                Same as git-force-push-this
   pit -c "msg"           Same as git-commit-all
   pit -r                 Same as git-reset-this
+  pit -b "name"          Same as git-new-branch
+  pit -m "name"          Same as git-modify-branch
   pit -h                 Show this help message
 
 USAGE EXAMPLES:
@@ -27,8 +33,17 @@ USAGE EXAMPLES:
   git-push-this
   pit -p
 
+  git-force-push-this
+  pit -pf
+
   git-reset-this
   pit -r
+
+  git-new-branch "feature/new-feature"
+  pit -b "feature/new-feature"
+
+  git-modify-branch "feature/updated-name"
+  pit -m "feature/updated-name"
 
 For more information, visit:
 https://github.com/parkjonghun/git-shortcut-for-pjh
@@ -63,6 +78,24 @@ gitpushthis() {
     git push origin "$current_branch" "$@"
 }
 
+# git force-push-this - Force push current branch to origin
+gitforcepushthis() {
+    local current_branch=$(git branch --show-current 2>/dev/null)
+    if [ -z "$current_branch" ]; then
+        echo "Error: Not in a git repository or no branch checked out"
+        return 1
+    fi
+    echo "Warning: This will force push to origin/$current_branch!"
+    read -p "Are you sure? (y/N) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        git push -f origin "$current_branch"
+    else
+        echo "Force push cancelled"
+        return 1
+    fi
+}
+
 # git commit-all - Add all changes and commit with message
 gitcommitall() {
     if [ -z "$1" ]; then
@@ -79,18 +112,41 @@ gitresetthis() {
     read -p "Are you sure? (y/N) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git reset --hard HEAD
+        git add . && git reset --hard HEAD
     else
         echo "Reset cancelled"
         return 1
     fi
 }
 
+# git new-branch - Create and switch to a new branch
+gitnewbranch() {
+    if [ -z "$1" ]; then
+        echo "Error: Branch name required"
+        echo "Usage: git-new-branch \"branch-name\""
+        return 1
+    fi
+    git switch -c "$1"
+}
+
+# git modify-branch - Rename current branch
+gitmodifybranch() {
+    if [ -z "$1" ]; then
+        echo "Error: Branch name required"
+        echo "Usage: git-modify-branch \"branch-name\""
+        return 1
+    fi
+    git branch -M "$1"
+}
+
 # Aliases for 'git-' prefix usage
 alias git-setup='gitsetup'
 alias git-push-this='gitpushthis'
+alias git-force-push-this='gitforcepushthis'
 alias git-commit-all='gitcommitall'
 alias git-reset-this='gitresetthis'
+alias git-new-branch='gitnewbranch'
+alias git-modify-branch='gitmodifybranch'
 
 # Shorter aliases using 'pit' prefix
 pit() {
@@ -106,6 +162,10 @@ pit() {
             shift
             gitpushthis "$@"
             ;;
+        -pf)
+            shift
+            gitforcepushthis "$@"
+            ;;
         -c)
             shift
             gitcommitall "$@"
@@ -113,6 +173,14 @@ pit() {
         -r)
             shift
             gitresetthis "$@"
+            ;;
+        -b)
+            shift
+            gitnewbranch "$@"
+            ;;
+        -m)
+            shift
+            gitmodifybranch "$@"
             ;;
         *)
             git "$@"
